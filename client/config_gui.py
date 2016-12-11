43412b1caf25fbd372343bd83353eb26970151b9
Daniel Foley <daniel@foley.life>
2016-12-11 16:22:37 -0600

    Cleaned up config_gui

--- client/config_gui.py
+++ client/config_gui.py
@@ -1,11 +1,8 @@
-import Tkinter
-import Tkinter as tk  # gives tk namespace
 from Tkinter import *
 from PIL import ImageTk, Image
 from ScrolledText import *
 #import tkFileDialog
 import tkMessageBox
-#import socket
 import ConfigParser
 import admin
 import xml.etree.cElementTree as ET
@@ -67,9 +64,9 @@ retention = ConfigSectionMap("main")['retention']
 runHour = ConfigSectionMap("main")['runhour']
 runMin = ConfigSectionMap("main")['runmin']
 
-class backup_gui(Tkinter.Tk):
+class backup_gui(Tk):
     def __init__(self,parent):
-        Tkinter.Tk.__init__(self,parent)
+        Tk.__init__(self,parent)
         self.parent = parent
         self.initialize()
 
@@ -77,118 +74,118 @@ class backup_gui(Tkinter.Tk):
         self.grid()
         
         ### Configuration - Hostname ###
-        self.config_name_label = Tkinter.Label(self, text="Hostname: ").grid(column=0,row=0,sticky='EW')
-        self.config_nameVariable = Tkinter.StringVar()
-        self.config_name = Tkinter.Entry(self,textvariable=self.config_nameVariable)
+        self.config_name_label = Label(self, text="Hostname: ").grid(column=0,row=0,sticky='EW')
+        self.config_nameVariable = StringVar()
+        self.config_name = Entry(self,textvariable=self.config_nameVariable)
         self.config_name.grid(column=1,row=0,sticky='EW')
         self.config_name.bind("<Return>", self.ConfigOnPressEnter)
         self.config_nameVariable.set(clientname)
         
         ### Configuration - Server ###
-        self.config_server_label = Tkinter.Label(self, text="Server: ").grid(column=0,row=1,sticky='EW')
-        self.config_serverVariable = Tkinter.StringVar()
-        self.config_server = Tkinter.Entry(self,textvariable=self.config_serverVariable)
+        self.config_server_label = Label(self, text="Server: ").grid(column=0,row=1,sticky='EW')
+        self.config_serverVariable = StringVar()
+        self.config_server = Entry(self,textvariable=self.config_serverVariable)
         self.config_server.grid(column=1,row=1,sticky='EW')
         self.config_server.bind("<Return>", self.ConfigOnPressEnter)
         self.config_serverVariable.set(HOST)
         
         ### Configuration - Port ###
-        self.config_port_label = Tkinter.Label(self, text="Port: ").grid(column=0,row=2,sticky='EW')
-        self.config_portVariable = Tkinter.StringVar()
-        self.config_port = Tkinter.Entry(self,textvariable=self.config_portVariable)
+        self.config_port_label = Label(self, text="Port: ").grid(column=0,row=2,sticky='EW')
+        self.config_portVariable = StringVar()
+        self.config_port = Entry(self,textvariable=self.config_portVariable)
         self.config_port.grid(column=1,row=2,sticky='EW')
         self.config_port.bind("<Return>", self.ConfigOnPressEnter)
         self.config_portVariable.set(PORT)
         
         ### Configuration - Password ###
         ### Need to add hash function, to store hash in config instead of plain text
-        self.config_password_label = Tkinter.Label(self, text="Password: ").grid(column=0,row=3,sticky='EW')
-        self.config_passwordVariable = Tkinter.StringVar()
-        self.config_password = Tkinter.Entry(self,show='*',textvariable=self.config_passwordVariable)
+        self.config_password_label = Label(self, text="Password: ").grid(column=0,row=3,sticky='EW')
+        self.config_passwordVariable = StringVar()
+        self.config_password = Entry(self,show='*',textvariable=self.config_passwordVariable)
         self.config_password.grid(column=1,row=3,sticky='EW')
         self.config_password.bind("<Return>", self.ConfigOnPressEnter)
         self.config_passwordVariable.set(password)
         
-        configbutton = Tkinter.Button(self,text=u"Save Config",
+        configbutton = Button(self,text=u"Save Config",
                             command=self.ConfigOnButtonClick)
         configbutton.grid(column=1,row=8)
         
         ### Configuration - Retention ###
-        self.config_retention_label = Tkinter.Label(self, text="Retention (Days): ").grid(column=0,row=4,sticky='EW')
-        self.config_retentionVariable = Tkinter.StringVar()
-        self.config_retention = Tkinter.Entry(self,textvariable=self.config_retentionVariable)
+        self.config_retention_label = Label(self, text="Retention (Days): ").grid(column=0,row=4,sticky='EW')
+        self.config_retentionVariable = StringVar()
+        self.config_retention = Entry(self,textvariable=self.config_retentionVariable)
         self.config_retention.grid(column=1,row=4,sticky='EW')
         self.config_retention.bind("<Return>", self.ConfigOnPressEnter)
         self.config_retentionVariable.set(retention)
         
         ### Configuration - Task Next Run ###
-        self.config_nextrun_label = Tkinter.Label(self, text="Next Run: ").grid(column=0,row=5,sticky='EW')
-        self.config_nextrunVariable = Tkinter.StringVar()
-        self.config_nextrun = Tkinter.Label(self,textvariable=self.config_nextrunVariable)
+        self.config_nextrun_label = Label(self, text="Next Run: ").grid(column=0,row=5,sticky='EW')
+        self.config_nextrunVariable = StringVar()
+        self.config_nextrun = Label(self,textvariable=self.config_nextrunVariable)
         self.config_nextrun.grid(column=1,row=5,sticky='EW')
         #self.config_nextrunVar = query_windows_task()
         self.config_nextrunVariable.set(query_windows_task())
         
         ### Configuration - Task Next Run Hour ###
-        self.config_nextrunHour_label = Tkinter.Label(self, text="Hour: ").grid(column=0,row=6,sticky='EW')
-        self.config_nextrunHourVariable = Tkinter.StringVar()
-        self.config_nextrunHour = Tkinter.Entry(self,textvariable=self.config_nextrunHourVariable)
+        self.config_nextrunHour_label = Label(self, text="Hour: ").grid(column=0,row=6,sticky='EW')
+        self.config_nextrunHourVariable = StringVar()
+        self.config_nextrunHour = Entry(self,textvariable=self.config_nextrunHourVariable)
         self.config_nextrunHour.grid(column=1,row=6,sticky='EW')
         self.config_nextrun.bind("<Return>", self.ConfigOnPressEnter)
         self.config_nextrunHourVariable.set(runHour)
         
         ### Configuration - Task Next Run Minute ###
-        self.config_nextrunMin_label = Tkinter.Label(self, text="Minute: ").grid(column=0,row=7,sticky='EW')
-        self.config_nextrunMinVariable = Tkinter.StringVar()
-        self.config_nextrunMin = Tkinter.Entry(self,textvariable=self.config_nextrunMinVariable)
+        self.config_nextrunMin_label = Label(self, text="Minute: ").grid(column=0,row=7,sticky='EW')
+        self.config_nextrunMinVariable = StringVar()
+        self.config_nextrunMin = Entry(self,textvariable=self.config_nextrunMinVariable)
         self.config_nextrunMin.grid(column=1,row=7,sticky='EW')
         self.config_nextrun.bind("<Return>", self.ConfigOnPressEnter)
         self.config_nextrunMinVariable.set(runMin)
         
         ### Configuration - Backup List ###
         # create the listbox (note that size is in characters)
-        #task_list = tk.Listbox(root, width=50, height=6)
+        #task_list = Listbox(root, width=50, height=6)
         self.textPad = ScrolledText(self, width=100, height=30)
-        self.task_list = tk.Listbox(self, width=50, height=6)
+        self.task_list = Listbox(self, width=50, height=6)
         self.task_list.grid(row=8, column=4)
 
         # create a vertical scrollbar to the right of the listbox
-        self.yscroll = tk.Scrollbar(command=self.task_list.yview, orient=tk.VERTICAL)
-        self.yscroll.grid(row=8, column=5, sticky=tk.N+tk.S)
+        self.yscroll = Scrollbar(command=self.task_list.yview, orient=VERTICAL)
+        self.yscroll.grid(row=8, column=5, sticky=N+S)
         self.task_list.configure(yscrollcommand=self.yscroll.set)
         
         ### Action - Upload ###
-#        self.action_put_label = Tkinter.Label(self, text="File: ").grid(column=0,row=3,sticky='EW')
-#        self.action_putVariable = Tkinter.StringVar()
-#        self.action_put = Tkinter.Entry(self,textvariable=self.action_putVariable)
+#        self.action_put_label = Label(self, text="File: ").grid(column=0,row=3,sticky='EW')
+#        self.action_putVariable = StringVar()
+#        self.action_put = Entry(self,textvariable=self.action_putVariable)
 #        self.action_put.grid(column=1,row=3,sticky='EW')
 #        self.action_put.bind("<Return>", self.ActionPutOnPressEnter)
 #        self.action_putVariable.set(u"Enter File Name")
 #        
-#        action_put_button = Tkinter.Button(self,text=u"Upload",
+#        action_put_button = Button(self,text=u"Upload",
 #                              command=self.ActionPutOnButtonClick)
 #        action_put_button.grid(column=2,row=3)
 
         # use entry widget to display/edit selection
-        self.input = tk.Entry(self, width=50)
+        self.input = Entry(self, width=50)
         self.input.insert(0, 'Add location')
         self.input.grid(row=6, column=4)
         # pressing the enter key will update edited line
         self.input.bind('<Return>', self.set_list)
         
-        self.button_enable_schedule = tk.Button(self, text=' Enable Schedule', command=self.enable_schedule)
-        self.button_enable_schedule.grid(row=5, column=3, sticky=tk.E)
+        self.button_enable_schedule = Button(self, text=' Enable Schedule', command=self.enable_schedule)
+        self.button_enable_schedule.grid(row=5, column=3, sticky=E)
         
-        self.button_disable_schedule = tk.Button(self, text='Disable Schedule', command=self.disable_schedule)
-        self.button_disable_schedule.grid(row=6, column=3, sticky=tk.E)
+        self.button_disable_schedule = Button(self, text='Disable Schedule', command=self.disable_schedule)
+        self.button_disable_schedule.grid(row=6, column=3, sticky=E)
 
         #This button is used to add tasks
-        self.button_add_task = tk.Button(self, text='  Add ', command=self.new_task)
-        self.button_add_task.grid(row=5, column=4, sticky=tk.E)
+        self.button_add_task = Button(self, text='  Add ', command=self.new_task)
+        self.button_add_task.grid(row=5, column=4, sticky=E)
 
         #This Button is used to call the delete function
-        self.button_delete = tk.Button(self, text='Delete', command=self.delete_item)
-        self.button_delete.grid(row=7, column=4, sticky=tk.E)
+        self.button_delete = Button(self, text='Delete', command=self.delete_item)
+        self.button_delete.grid(row=7, column=4, sticky=E)
 
         # left mouse click on a list item to display selection
         self.task_list.bind('<ButtonRelease-1>', self.get_list)
@@ -198,7 +195,7 @@ class backup_gui(Tkinter.Tk):
         self.update()
         self.geometry(self.geometry())
         self.config_name.focus_set()
-        self.config_name.selection_range(0, Tkinter.END)
+        self.config_name.selection_range(0, END)
         
     def update_vars(self):
         Config.read('config/config.ini')
@@ -222,14 +219,14 @@ class backup_gui(Tkinter.Tk):
         self.save_config()
         self.update_vars()
         self.config_name.focus_set()
-        self.config_name.selection_range(0, Tkinter.END)
+        self.config_name.selection_range(0, END)
 
     def ConfigOnPressEnter(self,event):
         self.save_tasks()
         self.save_config()
         self.update_vars()
         self.config_name.focus_set()
-        self.config_name.selection_range(0, Tkinter.END)
+        self.config_name.selection_range(0, END)
         
     def enable_schedule(self):
         create_windows_task(self.config_nextrunHour.get(), self.config_nextrunMin.get())
@@ -245,7 +242,7 @@ class backup_gui(Tkinter.Tk):
         if fin is not None: 
             task_listread = fin.readlines() 
         for item in task_listread: 
-            self.task_list.insert(tk.END, item) 
+            self.task_list.insert(END, item) 
         fin.close()
 
     def exit(self):
@@ -257,7 +254,7 @@ class backup_gui(Tkinter.Tk):
 
 
     def new_task(self):
-        self.task_list.insert(tk.END, self.input.get())
+        self.task_list.insert(END, self.input.get())
 
 
     def delete_item(self):
@@ -296,7 +293,7 @@ class backup_gui(Tkinter.Tk):
             # delete old listbox line
             self.task_list.delete(index)
         except IndexError:
-            index = tk.END
+            index = END
             # insert edited item back into task_list at index
             self.task_list.insert(index, self.input.get())
 
@@ -306,7 +303,7 @@ class backup_gui(Tkinter.Tk):
         save the current listbox contents to a file
         """
         # get a list of listbox lines
-        temp_list = list(self.task_list.get(0, tk.END))
+        temp_list = list(self.task_list.get(0, END))
         # add a trailing newline char to each line, but remove extisting so not to duplicate newlines
         temp_list = [task.strip() + '\n' for task in temp_list]
         if temp_list is not None:
@@ -346,7 +343,7 @@ if __name__ == "__main__":
     app = backup_gui(None)
     app.title('rPyBackup Configuration')
     
-    menu = Tkinter.Menu(app)
+    menu = Menu(app)
     app.config(menu=menu)
     
     filemenu = Menu(menu)
@@ -354,12 +351,12 @@ if __name__ == "__main__":
     filemenu.add_separator()
     filemenu.add_command(label="Exit", command=exit)
     
-    helpmenu = Tkinter.Menu(menu)
+    helpmenu = Menu(menu)
     menu.add_cascade(label="Help", menu=helpmenu)
     helpmenu.add_command(label="About ", command=app.about)
     
     textPad = ScrolledText(app, width=100, height=30)
-    task_list = tk.Listbox(app, width=50, height=6)
+    task_list = Listbox(app, width=50, height=6)
     logo = ImageTk.PhotoImage(Image.open("images/logo.png"))
     imglabel = Label(app, image=logo).grid(row=0, column=4, rowspan=6,columnspan=3)
     app.iconbitmap('images/favicon.ico')
